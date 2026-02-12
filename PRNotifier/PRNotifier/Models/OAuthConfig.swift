38586a9ba8e4b0283d26308fdacb5a4ae4c56600
Nick Strayer <nick.strayer@posit.co>
2026-02-12 13:29:00 -0500

    chore: set GitHub OAuth client ID

--- PRNotifier/PRNotifier/Models/OAuthConfig.swift
+++ PRNotifier/PRNotifier/Models/OAuthConfig.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum OAuthConfig {
-    static let clientID = "YOUR_CLIENT_ID" // Replace with registered OAuth App client ID
+    static let clientID = "Ov23li0e346jRMzotPpO"
     static let scope = "repo"
     static let deviceCodeURL = "https://github.com/login/device/code"
     static let accessTokenURL = "https://github.com/login/oauth/access_token"
