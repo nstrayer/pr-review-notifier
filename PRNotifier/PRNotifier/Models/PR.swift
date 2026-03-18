c61f7eb54ca206bf3750d90e300558a6f00c0d14
Nick Strayer <nick.strayer@posit.co>
2026-03-18 12:08:25 -0400

    feat: add ciInfo and isReadyToMerge to PR model
    Adds optional CIInfo field to PR and a computed isReadyToMerge property
    that returns true when CI is passing and at least one review is approved.
    Also registers CIInfo.swift in the Xcode project file so it compiles.

--- PRNotifier/PRNotifier/Models/PR.swift
+++ PRNotifier/PRNotifier/Models/PR.swift
@@ -9,10 +9,17 @@ struct PR: Codable, Equatable, Identifiable {
     let authorLogin: String?
     var reviews: [ReviewInfo]?
     var isAuthored: Bool?
+    var ciInfo: CIInfo?
 
     enum CodingKeys: String, CodingKey {
         case id, number, title
         case htmlURL = "html_url"
-        case repo, authorLogin, reviews, isAuthored
+        case repo, authorLogin, reviews, isAuthored, ciInfo
+    }
+
+    var isReadyToMerge: Bool {
+        guard let ci = ciInfo, ci.overallStatus == .passing else { return false }
+        guard let reviews = reviews else { return false }
+        return reviews.contains { $0.state == .approved }
     }
 }
